0a3e34fabc5663542fc8a3bbb5dae8b868654cc8
MaquinoBSC <martinare.ipn@gmail.com>
2021-07-05 21:32:23 -0500

    instalar Bootstrap Vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-button variant="outline-primary">Button</b-button>
   </div>
 </template>
 
