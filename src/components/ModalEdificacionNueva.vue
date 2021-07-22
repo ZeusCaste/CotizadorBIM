8fea485bfa68414ba9973dde5a2e6c104e17a220
MaquinoBSC <martinare.ipn@gmail.com>
2021-07-21 21:27:19 -0500

    agregar formulario en la subseccion tipo de edificacion

--- src/components/ModalEdificacionNueva.vue
+++ src/components/ModalEdificacionNueva.vue
@@ -37,20 +37,52 @@
                 <p class="font-weight-bold text-white ml-3 mt-2">Tipo de Edificacion: </p>
             </div>
             <div class="subSeccion">
-                <div class="build w-50">
-                    <b-icon
-                        icon="check-circle"
-                        scale="1.5"
-                    ></b-icon>
-                    <p class="ml-3 mt-3">Edificacion</p>
+                <div class="w-50">
+                    <div class="build">
+                        <b-icon
+                            icon="check-circle"
+                            scale="1.5"
+                        ></b-icon>
+                        <p class="ml-3 mt-3 font-weight-bold">Edificacion</p>
+                    </div>
+                    {{form.edificacion}}
+                    <div class="radio-Container py-4 d-flex justify-content-center">
+                        <b-form-radio-group
+                            v-model="form.edificacion"
+                            :options="edificacionOptions"
+                            name="text"
+                            stacked
+                        ></b-form-radio-group>
+                    </div>
                 </div>
-                <div class="build w-50">
-                    <b-icon
-                        icon="ui-checks"
-                        scale="1.5"
-                    ></b-icon>
-                    <p class="ml-3 mt-3">Proyectos y/o Estudios</p>
+                <div class="w-50">
+                    <div class="build">
+                        <b-icon
+                            icon="ui-checks"
+                            scale="1.5"
+                        ></b-icon>
+                        <p class="ml-3 mt-3 font-weight-bold">Proyectos y/o Estudios</p>
+                    </div>
+                    {{form.proyectos_estudios}}
+                    <div class="radio-Container py-4 d-flex justify-content-center">
+                        <b-form-checkbox-group
+                            v-model="form.proyectos_estudios"
+                            :options="proyectos_estudiosOptions"
+                            name="text"
+                            stacked
+                        ></b-form-checkbox-group>
+                    </div>
                 </div>
+            </div>
+            <div class="button-container">
+                <b-button 
+                    variant="success"
+                    @click="addTipoEdificacion()"
+                >Agregar</b-button>
+                <b-button 
+                    variant="danger"
+                    @click="cleanFields()"
+                >Limpiar Campos</b-button>
             </div>
         </div>
         <div>
@@ -95,7 +127,33 @@ export default {
     },
     data(){
         return{
-            
+            form: {
+                edificacion: null,
+                proyectos_estudios: [],
+            },
+            edificacionOptions: [
+                {id: 1, text: "Vivienda Familiar", value: "vivienda_familiar"},
+                {id: 2, text: "Vivienda Adosada", value: "vivienda_adosada"},
+                {id: 3, text: "Vivienda Multifamiliar", value: "vivienda_multifamiliar"},
+                {id: 4, text: "Vivienda Residencial", value: "vivienda_residencial"},
+                {id: 5, text: "Oficinas y Locales", value: "oficinas_locales"},
+                {id: 6, text: "Comercial", value: "comercial"},
+                {id: 7, text: "Administrativo", value: "administrativo"},
+                {id: 8, text: "Estacionamientos", value: "estacionamientos"},
+                {id: 9, text: "Pública concurrencia", value: "publica_concurrencia"},
+                {id: 10, text: "Docencia", value: "docencia"},
+                {id: 11, text: "Salud", value: "salud"},
+                {id: 12, text: "Industrial", value: "industrial"},
+            ],
+            proyectos_estudiosOptions: [
+                {id: 1, text: "Arquitectura", value: "arquitectura"},
+                {id: 1, text: "Instalación hodrosanitaria", value: "instalacion_hidrosanitaria"},
+                {id: 1, text: "Instalación Eléctrica", value: "instalacion_electrica"},
+                {id: 1, text: "Aire acondicionado sin balance térmico", value: "arire_acondicionado_sin_balance_termico"},
+                {id: 1, text: "Aire acondicionado con balance térmico", value: "arire_acondicionado_con_balance_termico"},
+                {id: 1, text: "Ventilación y Extracción", value: "ventilacion_y_extraccion"},
+                {id: 1, text: "Voz y Datos", value: "voz_y_datos"},
+            ]
         }
     },
     methods: {
@@ -111,6 +169,15 @@ export default {
             if(typeof this.$refs["edificacion-nueva"] !== "undefined"){
                 this.$refs["edificacion-nueva"].hide();
             }
+        },
+        cleanFields(){
+            this.form= {
+                edificacion: null,
+                proyectos_estudios: [],
+            }
+        },
+        addTipoEdificacion(){
+            console.log(this.form);
         }
     },
 }
@@ -163,4 +230,14 @@ export default {
         justify-content: center;
     }
 
+    .radio-Container{
+        display: flex;
+        justify-content: center;
+    }
+
+    .button-container{
+        display: flex;
+        justify-content: space-around;
+        margin-bottom: 15px;
+    }
 </style>
