140c3fdb174d96f187b47985192bbac9ff5dba25
MaquinoBSC <martinare.ipn@gmail.com>
2021-07-28 21:54:13 -0500

    formulario terminado para el modal de edificacion nueva

--- src/components/ModalEdificacionNueva.vue
+++ src/components/ModalEdificacionNueva.vue
@@ -99,6 +99,7 @@
                             <b-icon
                                 icon="geo-alt-fill"
                                 scale="1"
+                                class="mr-1"
                             ></b-icon>
                             <label>Ubicación del proyecto</label>
                             <b-form-input placeholder="Calle 6 No. 517, Colonia Pascal"></b-form-input>
@@ -111,6 +112,7 @@
                             <b-icon
                                 icon="geo-alt-fill"
                                 scale="1"
+                                class="mr-1"
                             ></b-icon>
                             <label for="estado">Estado</label>
                             <b-form-select
@@ -130,12 +132,14 @@
                             <b-icon
                                 icon="geo-alt-fill"
                                 scale="1"
+                                class="mr-1"
                             ></b-icon>
                             <label for="municipio">Ciudad o Municipio</label>
                             <b-form-select
                                 id="minucipio"
                                 v-model="form.municipio"
                                 :options="municipiosSelected"
+                                :disabled="setDisabledMunicipiosSelect"
                             ></b-form-select>
                         </b-col>
                     </b-row>
@@ -146,6 +150,7 @@
                             <b-icon
                                 icon="geo-alt-fill"
                                 scale="1"
+                                class="mr-1"
                             ></b-icon>
                             <label for="">Codigo Postal</label>
                             <b-form-input
@@ -155,25 +160,24 @@
                     </b-row>
                 </div>
             </div>
-            <div 
-                class="subseccion bg-dark row justify-content-center align-items-center m-auto pb-1 w-50" 
-                style="height: 40px; border-radius: 15px"
-            >
-                <b-icon
-                    icon="columns-gap"
-                    variant="white"
-                    scale="1.35"
-                    style="margin-top: -8px"
-                ></b-icon>
-                <p class="text-white ml-2 mt-2">Areas del Proyecto</p>
+            <div class="button-toggle">
+                <b-button 
+                    variant="dark"
+                    size="md"
+                    class="w-50 button"
+                >
+                    <b-icon icon="columns-gap"></b-icon>
+                    Areas del Proyecto
+                </b-button>
             </div>
-            <div class="container build m-3">
+            <div class="container build my-3">
                 <div class="input-area-proyecto">
                     <b-row class="mx-2">
                         <b-col>
                             <b-icon
-                                icon="geo-alt-fill"
+                                icon="bounding-box"
                                 scale="1"
+                                class="mr-1"
                             ></b-icon>
                             <label for="">Área de planta baja (m2)</label>
                             <b-form-input
@@ -183,12 +187,13 @@
                         </b-col>
                     </b-row>
                 </div>
-                <div class="w-25">
+                <div class="input-area-proyecto">
                     <b-row class="mx-2">
                         <b-col>
                             <b-icon
-                                icon="geo-alt-fill"
+                                icon="bricks"
                                 scale="1"
+                                class="mr-1"
                             ></b-icon>
                             <label for="">Número de niveles tipo</label>
                             <b-form-input
@@ -198,12 +203,13 @@
                         </b-col>
                     </b-row>
                 </div>
-                <div class="w-25">
+                <div class="input-area-proyecto">
                     <b-row class="mx-2">
                         <b-col>
                             <b-icon
-                                icon="geo-alt-fill"
+                                icon="bounding-box-circles"
                                 scale="1"
+                                class="mr-1"
                             ></b-icon>
                             <label for="">Área del nivel tipo (m2)</label>
                             <b-form-input
@@ -214,6 +220,94 @@
                     </b-row>
                 </div>
             </div>
+            <div class="button-toggle">
+                <b-button 
+                    variant="dark"
+                    size="md"
+                    class="w-50 button"
+                    @click="sotanosToggle()"
+                >
+                    <b-icon icon="grid-1x2"></b-icon>
+                    Sotanos
+                    <b-icon :icon="sotanos ? 'caret-up-square' : 'caret-down-square'"></b-icon>
+                </b-button>
+            </div>
+            <div class="build" v-if="sotanos">
+                <div class="input-area-proyecto">
+                    <b-row class="mx-2">
+                        <b-col>
+                            <b-icon
+                                icon="geo-alt-fill"
+                                scale="1"
+                            ></b-icon>
+                            <label for="">Número de sotanos</label>
+                            <b-form-input
+                                v-model="form.numeroSotanos"
+                                placeholder="2"
+                            ></b-form-input>
+                        </b-col>
+                    </b-row>
+                </div>
+                <div class="input-area-proyecto">
+                    <b-row class="mx-2">
+                        <b-col>
+                            <b-icon
+                                icon="geo-alt-fill"
+                                scale="1"
+                            ></b-icon>
+                            <label for="">Área de sotano (m2)</label>
+                            <b-form-input
+                                v-model="form.areaSotano"
+                                placeholder="Introduce el area en m2"
+                            ></b-form-input>
+                        </b-col>
+                    </b-row>
+                </div>
+            </div>
+            <div class="w-100 row my-4 d-flex justify-content-around">
+                <div class="accesibilidad">
+                    <div class="row d-flex justify-content-center">
+                        <b-icon icon="app-indicator" class="mt-1 mr-2"></b-icon>
+                        <p class="font-weight-bold">Accesibilidad</p>
+                    </div>
+                    <div class="radio-Container py-1 d-flex justify-content-center">
+                        <b-form-radio-group
+                            v-model="form.accesibilidad"
+                            :options="accesibilidadOptions"
+                            name="text"
+                            stacked
+                        ></b-form-radio-group>
+                    </div>
+                </div>
+                <div class="accesibilidad">
+                    <div class="row d-flex justify-content-center">
+                        <b-icon icon="image-fill" class="mt-1 mr-2"></b-icon>
+                        <p class="font-weight-bold">Topografia</p>
+                    </div>
+                    <div class="radio-Container py-1 d-flex justify-content-center">
+                        <b-form-radio-group
+                            v-model="form.topografia"
+                            :options="topografiaOptions"
+                            name="text"
+                            stacked
+                        ></b-form-radio-group>
+                    </div>
+                </div>
+                <div class="accesibilidad">
+                    <div class="row d-flex justify-content-center">
+                        <b-icon icon="geo-fill" class="mt-1 mr-2"></b-icon>
+                        <p class="font-weight-bold">Ubicación</p>
+                    </div>
+                    <div class="radio-Container py-1 d-flex justify-content-center">
+                        <b-form-radio-group
+                            v-model="form.ubicacion"
+                            :options="ubicacionOptions"
+                            name="text"
+                            stacked
+                        ></b-form-radio-group>
+                    </div>
+                </div>
+            </div>
         </div>
         <div>
             <div class="seccion bg-info">
@@ -221,8 +315,59 @@
                     icon="person-badge"
                     variant="white"
                     font-scale="2.5"
+                    class="mr-1"
                 ></b-icon>
                 <p class="font-weight-bold text-white ml-3 mt-2">Datos del Contacto: </p>
+            </div>
+            <div class="container build my-3">
+                <div class="datos-contacto">
+                    <b-row class="mx-2">
+                        <b-col>
+                            <b-icon
+                                icon="person-bounding-box"
+                                scale="1"
+                                class="mr-1"
+                            ></b-icon>
+                            <label for="">Nombre completo</label>
+                            <b-form-input
+                                v-model="form.datos_contacto.nombreCompleto"
+                                placeholder="Francisco Martinez Del Campo"
+                            ></b-form-input>
+                        </b-col>
+                    </b-row>
+                </div>
+                <div class="datos-contacto">
+                    <b-row class="mx-2">
+                        <b-col>
+                            <b-icon
+                                icon="phone-fill"
+                                scale="1"
+                                class="mr-1"
+                            ></b-icon>
+                            <label for="">Número de telefono</label>
+                            <b-form-input
+                                v-model="form.datos_contacto.numeroTelefono"
+                                placeholder="+1 3004005000"
+                            ></b-form-input>
+                        </b-col>
+                    </b-row>
+                </div>
+                <div class="datos-contacto">
+                    <b-row class="mx-2">
+                        <b-col>
+                            <b-icon
+                                icon="envelope-fill"
+                                scale="1"
+                                class="mr-1"
+                            ></b-icon>
+                            <label for="">Correo Electrónico</label>
+                            <b-form-input
+                                v-model="form.datos_contacto.correoElectronico"
+                                placeholder="ejemplo@ejemplo.com"
+                            ></b-form-input>
+                        </b-col>
+                    </b-row>
+                </div>
             </div>
         </div>
         <div class="footer">
@@ -260,6 +405,16 @@ export default {
                 areaPlantaBaja: '',
                 numeroNiveles: '',
                 areaNivelTipo: '',
+                numeroSotanos: '',
+                areaSotano: '',
+                accesibilidad: '',
+                topografia: '',
+                unicacion: '',
+                datos_contacto: {
+                    nombreCompleto: '',
+                    numeroTelefono: '',
+                    correoElectronico: '',
+                },
             },
             edificacionOptions: [
                 {id: 1, text: "Vivienda Familiar", value: "vivienda_familiar"},
@@ -277,16 +432,36 @@ export default {
             ],
             proyectos_estudiosOptions: [
                 {id: 1, text: "Arquitectura", value: "arquitectura"},
-                {id: 1, text: "Instalación hodrosanitaria", value: "instalacion_hidrosanitaria"},
-                {id: 1, text: "Instalación Eléctrica", value: "instalacion_electrica"},
-                {id: 1, text: "Aire acondicionado sin balance térmico", value: "arire_acondicionado_sin_balance_termico"},
-                {id: 1, text: "Aire acondicionado con balance térmico", value: "arire_acondicionado_con_balance_termico"},
-                {id: 1, text: "Ventilación y Extracción", value: "ventilacion_y_extraccion"},
-                {id: 1, text: "Voz y Datos", value: "voz_y_datos"},
+                {id: 2, text: "Instalación hodrosanitaria", value: "instalacion_hidrosanitaria"},
+                {id: 3, text: "Instalación Eléctrica", value: "instalacion_electrica"},
+                {id: 4, text: "Aire acondicionado sin balance térmico", value: "arire_acondicionado_sin_balance_termico"},
+                {id: 5, text: "Aire acondicionado con balance térmico", value: "arire_acondicionado_con_balance_termico"},
+                {id: 6, text: "Ventilación y Extracción", value: "ventilacion_y_extraccion"},
+                {id: 7, text: "Voz y Datos", value: "voz_y_datos"},
+            ],
+            accesibilidadOptions: [
+                {id: 1, text: "Muy buena", value: "muy_buena"},
+                {id: 2, text: "Buena", value: "buena"},
+                {id: 3, text: "Normal", value: "normal"},
+                {id: 4, text: "Dificil", value: "dificil"},
+                {id: 5, text: "Muy dificil", value: "muy_dificil"},
+            ],
+            topografiaOptions: [
+                {id: 1, text: "Plana", value: 'plana'},
+                {id: 2, text: "Con desnivel minimo", value: 'desnivel_minimo'},
+                {id: 3, text: "Con desnivel pronunciado", value: 'desnivel_pronunciado'},
+                {id: 4, text: "Accidentada", value: 'accidentada'},
+                {id: 5, text: "Muy accidentada", value: 'muy_accidentada'},
+            ],
+            ubicacionOptions: [
+                {id: 1, text: "Entre colindancias", value: 'entre_colindancias'},
+                {id: 2, text: "En esquina", value: 'en_esquina'},
+                {id: 3, text: "Aislada", value: 'aislada'},
             ],
             estados: estados,
             municipiosSelected: [],
             municipios: municipios,
+            sotanos: false,
         }
     },
     methods: {
@@ -328,8 +503,16 @@ export default {
         },
         addTipoEdificacion(){
             console.log(this.form);
+        },
+        sotanosToggle(){
+            this.sotanos= !this.sotanos;
         }
     },
+    computed: {
+        setDisabledMunicipiosSelect(){
+            return this.form.estado ? false : true;
+        }
+    }
 }
 </script>
 
@@ -391,7 +574,28 @@ export default {
         margin-bottom: 15px;
     }
 
-    .input-area-proyecto{
+    .input-area-proyecto, .datos-contacto{
+        width: 50%;
+        margin-top: 15px;
+    }
+
+    .button-toggle{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+
+    .button{
+        border-top-left-radius: 5px;
+        border-top-right-radius: 5px;
+        border-bottom-left-radius: 20px;
+        border-bottom-right-radius: 20px;
+    }
+
+    .accesibilidad{
         width: 30%;
     }
 </style>
