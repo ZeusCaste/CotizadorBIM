ea30c74ffbfd5fd9d393abe8f2ce4bfa12b920b8
MaquinoBSC <martinare.ipn@gmail.com>
2022-03-30 23:44:45 -0600

    Agregar estilos para menu seleccionado en el componente nabvarSession

--- src/components/layout/NabvarSession.vue
+++ src/components/layout/NabvarSession.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
         <b-nav vertical pills >
-            <div v-for="(menuComponent, idx) in menuComponents" :key="idx">
-                <b-button class="text-left mt-1" block variant="warning">{{ menuComponent }}</b-button>
+            <div v-for="(menuName, idx) in menuComponents" :key="idx">
+                <b-button 
+                    class="text-left mt-1 font-weight-bold shadow"
+                    :class="activedMenu == menuName ? 'actived-menu' : 'desactivedMenu'"
+                    variant="warning"
+                    block 
+                    @click="showContent(menuName)"
+                >
+                    {{ menuName }}
+                </b-button>
             </div>
-            <!-- {{ menuComponents }}
-            <b-nav-item active active-class="bg-info" class="bg-warning">Active</b-nav-item>
-            <b-nav-item>Link</b-nav-item>
-            <b-nav-item>Another Link</b-nav-item>
-            <b-nav-item disabled>Disabled</b-nav-item> -->
         </b-nav>
     </div>
 </template>
@@ -18,10 +21,26 @@ export default {
     name: "NabvarSession",
     props: {
         menuComponents: Array,
+    },
+    data(){
+        return {
+            activedMenu: '',
+        }
+    },
+    methods: {
+        showContent(newActivedMenu){
+            this.activedMenu= newActivedMenu;
+        }
     }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    .actived-menu {
+        border: 3px solid #000;
+    }
 
+    .desactivedMenu {
+        background: rgb(241, 208, 109);
+    }
 </style>
