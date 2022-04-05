1b876c6953d89a1abe138e66d16ef34a817c8d58
MaquinoBSC <martinare.ipn@gmail.com>
2022-04-05 14:00:00 +0100

    Componentizar el apartado de factores para la seccion de usuario administrador

---
+++ src/components/pages/AdminSession/Factors.vue
@@ -0,0 +1,221 @@
+<template>
+    <div>
+        <!-- <div class="d-flex flex-row-reverse my-4">
+            <div class="p-2 bd-highlight">
+                <b-button variant="dark" @click.prevent="logout()">
+                    Cerrar Sesión
+                    <b-icon icon="box-arrow-right"></b-icon>
+                </b-button>
+            </div>
+        </div> -->
+        <h1>Bienvenido administrador: {{ user && user.email }}</h1>
+        <table class="table table-responsive">
+            <tr><th>Factores Económicos</th>
+                <td>
+                    <label class="sr-only" for="inline-form-input-FE">Factor Situación de la Empresa</label>
+                    <b-form-input 
+                        id="inline-form-input-FE"
+                        v-model="form.economicFactors.companySituation"
+                        aria-describedby="FE"
+                        :readonly="!edit"
+                    ></b-form-input>
+                    <b-form-text id="FE"> Factor Situación de la Empresa </b-form-text>
+                </td>
+                <td>
+                    <label class="sr-only" for="inline-form-input-FM">Factor Mercado</label>
+                    <b-form-input 
+                        id="inline-form-input-FM" 
+                        aria-describedby="FM"
+                        v-model="form.economicFactors.market"
+                        :readonly="!edit"
+                    ></b-form-input>
+                    <b-form-text id="FM"> Factor Mercado </b-form-text>
+                </td>
+                <td>
+                    <label class="sr-only" for="inline-form-input-FR">Factor Reducción Tabulador</label>
+                    <b-form-input 
+                        id="inline-form-input-FR" 
+                        aria-describedby="FR"
+                        v-model="form.economicFactors.tabularReduction"
+                        :readonly="!edit"
+                    ></b-form-input>
+                    <b-form-text id="FR"> Factor Reducción Tabulador </b-form-text>
+                </td>
+            </tr>
+            <tr><th>Factores Tiempos de entrega</th>
+                <td>
+                    <label class="sr-only" for="inline-form-input-FT">Factor Tabulador</label>
+                    <b-form-input 
+                        id="inline-form-input-FT" 
+                        aria-describedby="FT"
+                        v-model="form.deliveryTimeFactors.tabuladorFactor2"
+                        :readonly="!edit"
+                    ></b-form-input>
+                    <b-form-text id="FT"> Factor Tabulador </b-form-text>
+                </td>
+                <td>
+                    <label class="sr-only" for="inline-form-input-FEE">Factor Eficencia de la Empresa</label>
+                    <b-form-input 
+                        id="inline-form-input-FEE" 
+                        aria-describedby="FEE"
+                        v-model="form.deliveryTimeFactors.efficiencyCompany"
+                        :readonly="!edit"
+                    ></b-form-input>
+                    <b-form-text id="FEE"> Factor Eficencia de la Empresa </b-form-text>
+                </td>
+                <td>
+                    <label class="sr-only" for="inline-form-input-FC">Factor Carga de trabajo Empresa</label>
+                    <b-form-input 
+                        id="inline-form-input-FC" 
+                        aria-describedby="FC"
+                        v-model="form.deliveryTimeFactors.companyWorkLoad"
+                        :readonly="!edit"
+                    ></b-form-input>
+                    <b-form-text id="FC"> Factor Carga de trabajo Empresa </b-form-text>
+                </td>
+            </tr>
+            <tr><th>Factores de Comisión</th>
+                <td>
+                    <label class="sr-only" for="inline-form-input-FCO">Factor Comisión</label>
+                    <b-form-input 
+                        id="inline-form-input-FCO" 
+                        aria-describedby="FCO"
+                        v-model="form.comissionFactors.comission"
+                        :readonly="!edit"
+                    ></b-form-input>
+                    <b-form-text id="FCO"> Factor Comisión </b-form-text>
+                </td>
+                <td>
+                    <label class="sr-only" for="inline-form-input-FCC">Factor Comisión Coordinador</label>
+                    <b-form-input 
+                        id="inline-form-input-FCC" 
+                        aria-describedby="FCC"
+                        v-model="form.comissionFactors.coordinatorComission"
+                        :readonly="!edit"
+                    ></b-form-input>
+                    <b-form-text id="FCC"> Factor Comisión Coordinador </b-form-text>
+                </td>
+            </tr>
+            <b-button 
+                class="mt-3"
+                type="submit" 
+                variant="info"
+                @click="editingFactors()"
+                size="lg"
+            >
+                {{ edit ? "Guardar" : "Editar" }}
+            <b-spinner v-if="spinner" variant="light"></b-spinner>
+            </b-button>
+        </table>
+        <div class="my-3">
+            <b-alert
+                :show="dismissCountDown"
+                dismissible
+                :variant="alertStyle"
+                @dismissed="dismissCountDown=0"
+                @dismiss-count-down="countDownChanged"
+            >
+                {{ error ? error : success }}
+            </b-alert>
+        </div>  
+    </div>
+</template>
+
+<script>
+import firebase from '../../../plugins/firebase';
+
+
+export default {
+    name: "Factors",
+    created(){
+        this.getFactors();
+    },
+    data(){
+        return {
+            edit: false,
+            error: null,
+            success: null,
+            dismissCountDown: 0,
+            spinner: false,
+            user: {},
+            form: {
+                economicFactors: {
+                    companySituation: 0,
+                    market: 0,
+                    tabularReduction: 0,
+                },
+                deliveryTimeFactors: {
+                    companyWorkLoad: 0,
+                    efficiencyCompany: 0,
+                    tabuladorFactor2: 0,
+                },
+                comissionFactors: {
+                    comission: 0,
+                    coordinatorComission: 0,
+                }
+            }
+        }
+    },
+    methods: {
+        onSubmit(event) {
+            event.preventDefault()
+            //firebase.auth().createUserWithEmailAndPassword(this.form.email,this.form.password);
+            // alert(JSON.stringify(this.form))
+        },
+        async logout(){
+            // await firebase.auth().signOut();
+            //alert(JSON.stringify(this.form))
+        },
+        async getFactors(){
+            try {
+                const factorsRef= firebase.firestore().collection('factors').doc('bQS31BEEdF9n5HHatkB9');
+                const factorsDoc= await factorsRef.get();
+
+                this.form= Object.assign(this.form, factorsDoc.data());
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async editingFactors(){
+            this.edit= !this.edit;
+            this.dismissCountDown= 0;
+            this.error= null;
+            this.success= null;
+
+            if(!this.edit){
+                this.spinner= true;
+                try {
+                    const editFactors= firebase.functions().httpsCallable('editFactors');
+                    const response= await editFactors(this.form);
+                    
+                    if(response.data.success){
+                    console.log('success');
+                    this.dismissCountDown= 7;
+                    this.success= response.data.msg;
+                    await this.getFactors();
+                    }
+                    this.spinner= false;
+
+                } catch (error) {
+                    this.dismissCountDown= 7;
+                    this.error= error.message;
+                    this.edit= true;
+                    this.spinner= false;
+                }
+            }        
+        },
+        countDownChanged(dismissCountDown){
+            this.dismissCountDown = dismissCountDown;
+        },
+    },
+    computed: {
+        alertStyle(){
+            return this.error !== null && this.success === null ? 'danger' : 'success'
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
