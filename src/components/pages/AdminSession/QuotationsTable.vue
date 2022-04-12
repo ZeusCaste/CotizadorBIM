6277c9c6101c109c85a8b35df19a9d60611e125d
MaquinoBSC <martinare.ipn@gmail.com>
2022-04-12 14:00:00 +0100

    Crear tabla quotations, obtener datos desde firestores y mostrarlos en la tabla, en la seccion de admin

--- src/components/pages/AdminSession/QuotationsTable.vue
+++ src/components/pages/AdminSession/QuotationsTable.vue
@@ -1,12 +1,50 @@
 <template>
     <div>
-        Quotations
+        <b-table
+            :fields="fields"
+            :items="items"
+        >
+
+        </b-table>
     </div>
 </template>
 
 <script>
-export default {
+import firebase from '../../../plugins/firebase';
 
+export default {
+    name: "QuotationsTable",
+    data(){
+        return{
+            fields: [
+                {'label': "ID", 'key': 'id', 'sortable': false},
+                {'label': "Nombre", 'key': 'name', 'sortable': true},
+                {'label': "Correo", 'key': 'email', 'sortable': true},
+                {'label': "Fecha", 'key': 'date', 'sortable': true},
+                {'label': "Enlace", 'key': 'link', 'sortable': false},
+            ],
+            items: [],
+        }
+    },
+    created(){
+        this.getQuotations();
+    },
+    methods: {
+        getQuotations(){
+            try {
+                const ref= firebase.firestore().collection('quotations');
+                ref.onSnapshot((snapshot)=> {
+                    snapshot.forEach((doc)=> {
+                        // console.log("id: " + doc.id + " data: " + doc.data());
+                        this.items.push({ 'id': doc.id, ...doc.data() });
+                    })
+                })
+            } catch (error) {
+                console.log(error);
+            }
+
+        }
+    }
 }
 </script>
 
