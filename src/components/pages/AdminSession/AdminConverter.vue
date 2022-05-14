3ac677e2567024f7eb2e14202bd9b834e9bf82b6
MaquinoBSC <martinare.ipn@gmail.com>
2022-05-14 14:00:00 +0100

    se agrega el manejo de errores y spinner en la seccion de crear nuevo administrador

--- src/components/pages/AdminSession/AdminConverter.vue
+++ src/components/pages/AdminSession/AdminConverter.vue
@@ -4,9 +4,23 @@
             <b-row class="form-content">
                 <b-col>
                     <b-input placeholder="Email" v-model="email"></b-input>
-                    <b-button class="mt-3" variant="info" @click="converterToAdmin()">Convertir Administrador</b-button>
+                    <b-button class="mt-3" variant="info" @click="converterToAdmin()">
+                        Convertir Administrador
+                        <b-spinner small v-if="spinner" label="Spinning"></b-spinner>
+                    </b-button>
                 </b-col>
                 <b-col></b-col>
+            </b-row>
+            <b-row class="ml-4">
+                <b-alert 
+                    :show="dismissCountDown"
+                    dismissible
+                    :variant="success ? 'success' : 'danger'"
+                    @dismissed="dismissCountDown=0"
+                    @dismiss-count-down="countDownChanged"
+                >
+                    {{ responseMessage }}
+                </b-alert>
             </b-row>
         </div>
     </div>
@@ -21,23 +35,44 @@ export default {
     data(){
         return{
             email: '',
+            dismissSecs: 5,
+            dismissCountDown: 0,
+            success: '',
+            responseMessage: '',
+            spinner: false,
         }
     },
     methods: {
         async converterToAdmin(){
+            this.spinner= true;
+            this.error= '';
+
             if(!validateEmail(this.email)){
                 console.log("Email invalido");
+                this.success= false;
+                this.responseMessage= "Formato de correo electrónico invalido";
+                this.dismissCountDown = this.dismissSecs;
+                this.spinner= false;
                 return
             }
 
             try {
                 const createAdminUser= firebase.functions().httpsCallable('createAdminUser');
                 const response= await createAdminUser({ 'email': this.email, 'name': 'el maquino' });
-                console.log(response);
+                this.success= true;
+                this.responseMessage= response.data.message;
+                this.dismissCountDown = this.dismissSecs;
+                this.spinner= false;
             } catch (error) {
-                console.log(error);
+                this.success= false;
+                this.responseMessage= error;
+                this.dismissCountDown = this.dismissSecs;
+                this.spinner= false;
             }
-        }
+        },
+        countDownChanged(dismissCountDown) {
+            this.dismissCountDown = dismissCountDown
+        },
     }
 }
 </script>
