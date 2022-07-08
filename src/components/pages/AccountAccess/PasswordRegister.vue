5e4d038a0489458fe2ed774a7ce9a1e382954c2c
MaquinoBSC <martinare.ipn@gmail.com>
2022-07-07 22:04:15 -0500

    Se crea el componente para realizar el registro por email y contraseña

---
+++ src/components/pages/AccountAccess/PasswordRegister.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Registro con Correo y contraseña</h1>
+        <div>
+            <b-button variant="dark" @click="returnToRegisterMood()">Regresar a opciones de registro</b-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PasswordRegister',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        returnToRegisterMood(){
+            this.eventHub.$emit('change-mood', '');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/pages/AccountAccess/Register.vue
+++ src/components/pages/AccountAccess/Register.vue
@@ -1,50 +1,64 @@
 <template>
     <div class="container px-5 mb-5">
-        <p class="display-4 font-weight-bold text-dark mt-1 text-center mt-4">Únete a BIMTECH</p>
-        <div class="my-4 text-center d-flex flex-column">
-            <b-button variant="primary" class="my-2">
-                <b-icon icon="facebook" class="mx-2"></b-icon>
-                Continuar con Facebook
-            </b-button>
-            <b-button variant="danger" class="my-2" @click="signUpWithGoogle()">
-                <b-icon icon="google" class="mx-2"></b-icon>
-                Continuar con Google
-            </b-button>
-            <b-button variant="info" class="my-2" @click="signUpWithEmailAndPassword()">
-                <b-icon icon="at" class="mx-2"></b-icon>
-                Continua con cuenta email
-            </b-button>
+        <div v-if="!mood">
+            <p class="display-4 font-weight-bold text-dark mt-1 text-center mt-4">Únete a BIMTECH</p>
+            <h4 class="mt-5">Selecciona una forma de registro </h4>
+            <div class="my-4 text-center d-flex flex-column">
+                <b-button variant="primary" class="my-2">
+                    <b-icon icon="facebook" class="mx-2"></b-icon>
+                    Continuar con Facebook
+                </b-button>
+                <b-button variant="danger" class="my-2" @click="signUpWithGoogle()">
+                    <b-icon icon="google" class="mx-2"></b-icon>
+                    Continuar con Google
+                </b-button>
+                <b-button variant="info" class="my-2" @click="toEmailPasswordRegister()">
+                    <b-icon icon="at" class="mx-2"></b-icon>
+                    Continua con cuenta email
+                </b-button>
+            </div>
+            <div class="my-4">
+                <b-form-checkbox
+                    id="checkbox-1"
+                    v-model="termnsAndCondition"
+                    name="checkbox-1"
+                    value="accepted"
+                    unchecked-value="not_accepted"
+                >
+                    Acepto los términos y condiciones de las <a href="">politicas de privacidad</a>
+                </b-form-checkbox>
+            </div>
+            <div class="text-center mb-4">
+                ¿Tienes cuenta en BIMTECH?
+                <b-button 
+                    variant="link"
+                    @click="goToLogIn(true)"
+                >Inicia Sesión</b-button>
+            </div>
         </div>
-        <div class="my-4">
-            <b-form-checkbox
-                id="checkbox-1"
-                v-model="termnsAndCondition"
-                name="checkbox-1"
-                value="accepted"
-                unchecked-value="not_accepted"
-            >
-                Acepto los términos y condiciones de las <a href="">politicas de privacidad</a>
-            </b-form-checkbox>
-        </div>
-        <div class="text-center mb-4">
-            ¿Tienes cuenta en BIMTECH?
-            <b-button 
-                variant="link"
-                @click="goToLogIn(true)"
-            >Inicia Sesión</b-button>
-        </div>
+        <PasswordRegister v-if="mood === 'password'" />
     </div>
 </template>
 
 <script>
 import firebase from '../../../plugins/firebase';
+import PasswordRegister from './PasswordRegister.vue';
 
 
 export default {
+    name: 'Register',
+    components: {
+        PasswordRegister,
+    },
     data(){
         return {
-            termnsAndCondition: 'not_accepted'
+            termnsAndCondition: 'not_accepted',
+            mood: '',
         }
+    },
+    created(){
+        // Events
+        this.eventHub.$on('change-mood', this.changeMood);
     },
     methods: {
         goToLogIn(state){
@@ -56,10 +70,14 @@ export default {
                 .then((result)=> { console.log(result) })
                 .catch((err)=> { console.log(err) });
         },
-        signUpWithEmailAndPassword(){
-            firebase.auth().createUserWithEmailAndPassword('maquinola@gmail.com', 'abc123')
-                .then((response)=> { console.log(response) })
-                .catch((err)=> { console.log(err) })
+        toEmailPasswordRegister(){
+            this.mood = 'password';
+            // firebase.auth().createUserWithEmailAndPassword('maquinola@gmail.com', 'abc123')
+            //     .then((response)=> { console.log(response) })
+            //     .catch((err)=> { console.log(err) })
+        },
+        changeMood(){
+            this.mood = '';
         }
     }
 }
