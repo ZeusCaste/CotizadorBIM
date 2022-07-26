481358185dc3ebf724a2ccff81ab8c2d94565374
MaquinoBSC <martinare.ipn@gmail.com>
2022-07-25 22:13:18 -0500

    Diseño responsive cuenta de usuarios v1

--- src/components/layout/NabvarSession.vue
+++ src/components/layout/NabvarSession.vue
@@ -1,6 +1,35 @@
 <template>
     <div>
-        <b-nav vertical pills >
+        <nav class="navbar navbar-expand-lg">
+            <button 
+                class="btn btn-dark navbar-toggler" 
+                type="button" 
+                data-toggle="collapse" 
+                data-target="#navbarSupportedContent-user" 
+                aria-controls="navbarSupportedContent" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+            >
+                <b-icon variant="warning" icon="person-circle"></b-icon>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent-user">
+                <ul class="navbar-nav mr-auto row">
+                    <li class="nav-item w-100" v-for="(menuName, idx) in menuComponents" :key="idx">
+                        <b-button 
+                            class="text-left mt-1 font-weight-bold shadow"
+                            :class="selectedMenu == menuName ? 'actived-menu' : 'desactivedMenu'"
+                            variant="warning"
+                            block
+                            @click="showContent(menuName)"
+                        > 
+                            {{ menuName }}
+                        </b-button>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- <b-nav vertical pills >
             <div v-for="(menuName, idx) in menuComponents" :key="idx">
                 <b-button 
                     class="text-left mt-1 font-weight-bold shadow"
@@ -12,7 +41,7 @@
                     {{ menuName }}
                 </b-button>
             </div>
-        </b-nav>
+        </b-nav> -->
     </div>
 </template>
 
