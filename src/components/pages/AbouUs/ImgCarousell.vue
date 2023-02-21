5862169e31d933329f0dbbdabfeadc966e3f2bdc
MaquinoBSC <martinare.ipn@gmail.com>
2023-02-21 14:00:00 +0100

    Se eliminan recurso no necesarios

--- src/components/pages/AbouUs/ImgCarousell.vue
+++ src/components/pages/AbouUs/ImgCarousell.vue
@@ -21,39 +21,6 @@
             </div>
             <!-- <div class="carousel-item">
                 <img src="../../../media/images/mapsCarousell/map-0002.jpg" class="d-block w-75 img-fluid mx-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../media/images/mapsCarousell/map-0003.jpg" class="d-block w-75 img-fluid mx-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../media/images/mapsCarousell/map-0004.jpg" class="d-block w-75 img-fluid mx-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../media/images/mapsCarousell/map-0005.jpg" class="d-block w-75 img-fluid mx-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../media/images/mapsCarousell/map-0006.jpg" class="d-block w-75 img-fluid mx-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../media/images/mapsCarousell/map-0007.jpg" class="d-block w-75 img-fluid mx-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../media/images/mapsCarousell/map-0008.jpg" class="d-block w-75 img-fluid mx-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../media/images/mapsCarousell/map-0009.jpg" class="d-block w-75 img-fluid mx-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../media/images/mapsCarousell/map-0010.jpg" class="d-block w-75 img-fluid mx-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../media/images/mapsCarousell/map-0011.jpg" class="d-block w-75 img-fluid mx-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../media/images/mapsCarousell/map-0012.jpg" class="d-block w-75 img-fluid mx-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../media/images/mapsCarousell/map-00013.jpg" class="d-block w-75 img-fluid mx-auto" alt="...">
             </div> -->
         </div>
         <button class="carousel-control-prev btn btn-info" type="button" data-target="#carouselExampleFade" data-slide="prev">
