395768c56c2bb03f879c62ada16065c3388f321f
MaquinoBSC <martinare.ipn@gmail.com>
2023-08-07 23:56:24 -0600

    Agregar skeleton a la sesion de usuario tipo customer

--- src/views/CustomerSession.vue
+++ src/views/CustomerSession.vue
@@ -1,19 +1,27 @@
 <template>
-    <div class="row mx-3">
-        <div class="col-xl-1 col-lg-1 col-md-11 col-11 col-sm-11 mx-auto my-2 my-xl-5 my-lg-4 my-md-3 my-sm-2">
-            <NabvarSession :menuComponents="customerMenu"></NabvarSession>
+    <div>
+        <div v-if="!user" class="text-center">
+            <b-skeleton-icon
+                icon="person-fill"
+                :icon-props="{ fontScale: 35, variant: 'dark' }"
+            ></b-skeleton-icon>
         </div>
-        <div class="container col-xl-9 col-lg-8 col-md-11 col-11 col-sm-11 mx-auto my-2 my-xl-5 my-lg-4 my-md-3 my-sm-2 px-4">
-            <div class="d-flex justify-content-xl-around flex-md-column-reverse flex-sm-column-reverse flex-column-reverse flex-lg-row flex-xl-row">
-                <h1 class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">{{ selectedMenu }}</h1>
-                <h5 class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">Bienvenido <strong>{{ user && user.displayName }}</strong></h5>
+        <div class="row mx-3">
+            <div class="col-xl-1 col-lg-1 col-md-11 col-11 col-sm-11 mx-auto my-2 my-xl-5 my-lg-4 my-md-3 my-sm-2">
+                <NabvarSession :menuComponents="customerMenu"></NabvarSession>
             </div>
-            <!-- <Factores v-if="selectedMenu === 'Factores'" />
-            <Quotations v-if="selectedMenu === 'Cotizaciones Realizadas'" />
-            <Projects v-if="selectedMenu === 'Proyectos'" />
-            <Partners v-if="selectedMenu === 'Colaboradores'" />
-            <Customers v-if="selectedMenu === 'Clientes'" />
-            <AdminConverter v-if="selectedMenu === 'Convertir Administrador'" /> -->
+            <div class="container col-xl-9 col-lg-8 col-md-11 col-11 col-sm-11 mx-auto my-2 my-xl-5 my-lg-4 my-md-3 my-sm-2 px-4">
+                <div class="d-flex justify-content-xl-around flex-md-column-reverse flex-sm-column-reverse flex-column-reverse flex-lg-row flex-xl-row">
+                    <h1 class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">{{ selectedMenu }}</h1>
+                    <h5 class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">Bienvenido <strong>{{ user && user.displayName }}</strong></h5>
+                </div>
+                <!-- <Factores v-if="selectedMenu === 'Factores'" />
+                <Quotations v-if="selectedMenu === 'Cotizaciones Realizadas'" />
+                <Projects v-if="selectedMenu === 'Proyectos'" />
+                <Partners v-if="selectedMenu === 'Colaboradores'" />
+                <Customers v-if="selectedMenu === 'Clientes'" />
+                <AdminConverter v-if="selectedMenu === 'Convertir Administrador'" /> -->
+            </div>
         </div>
     </div>
 </template>
@@ -33,7 +41,7 @@ export default {
     },
     data() {
         return {
-            user: '',
+            user: null,
             customerMenu: [
                 'Mi Perfil',
                 'Mis Cotizaciones',
