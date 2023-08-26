c66a2bf29193e7d87d702b2560813533f9f78f72
MaquinoBSC <martinare.ipn@gmail.com>
2023-08-25 20:02:59 -0600

    Se agrega estilos a la sección 'Mi Perfil' de usuario partner p2

--- src/components/pages/PartnerSession/MyPerfilPartner.vue
+++ src/components/pages/PartnerSession/MyPerfilPartner.vue
@@ -1,41 +1,40 @@
 <template>
     <div class="mt-5">
         <div>
-            <p><strong>Name: </strong> {{ user && user.displayName }}</p>
-            <p><strong>Correo: </strong> {{ user && user.email }}</p>
-            <p><strong>Tipo de usuario: </strong> {{ user && user.customClaims.userType === 'partner' && 'Colaborador' }}</p>
-            <div class="mt-5">
-                <h2 class="mb-2">Datos Personales</h2>
+            <div class="user-data">
+                <p><strong>Name: </strong> {{ user && user.displayName }}</p>
+                <p><strong>Correo: </strong> {{ user && user.email }}</p>
+                <p><strong>Tipo de usuario: </strong> {{ user && user.customClaims.userType === 'partner' && 'Colaborador' }}</p>
+                <p><strong>Actividad Solicitada: </strong>{{ firestoreUserData && firestoreUserData.requestedActivity }}</p>
                 <p><strong>Fecha de Nacimiento: </strong>{{ firestoreUserData && firestoreUserData.bornDate }}</p>
                 <p><strong>CURP: </strong>{{ firestoreUserData && firestoreUserData.curp }}</p>
-                <p><strong>Actividad Solicitada: </strong>{{ firestoreUserData && firestoreUserData.requestedActivity }}</p>
-                <div>
+            </div>
+            <div class="mt-5">
+                <div class="user-data">
                     <h3 class="text-center">Dirección</h3>
-                    <div>
-                        <div class="row">
-                            <div class="col-4">
-                                <p><strong>Calle: </strong>{{ address && address.street }}</p>
-                            </div>
-                            <div class="col-4">
-                                <p><strong>Numero: </strong> Exterior: {{ address && address.extNumber }} Interior: {{ address && address.intNumber }}</p>
-                            </div>
-                            <div class="col-4">
-                                <p><strong>Colonia: </strong>{{ address && address.neighborhood }}</p>
-                            </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <p><strong>Calle: </strong>{{ address && address.street }}</p>
                         </div>
-                        <div class="row">
-                            <div class="col-3">
-                                <p><strong>Delegación: </strong>{{ address && address.delegation }}</p>
-                            </div>
-                            <div class="col-3">
-                                <p><strong>Ciudad: </strong>{{ address && address.city }}</p>
-                            </div>
-                            <div class="col-3">
-                                <p><strong>Estado: </strong>{{ address && address.state }}</p>
-                            </div>
-                            <div class="col-3">
-                                <p><strong>CP: </strong>{{ address && address.cp }}</p>
-                            </div>
+                        <div class="col-4">
+                            <p><strong>Numero: </strong> Exterior: {{ address && address.extNumber }} Interior: {{ address && address.intNumber }}</p>
+                        </div>
+                        <div class="col-4">
+                            <p><strong>Colonia: </strong>{{ address && address.neighborhood }}</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-3">
+                            <p><strong>Delegación: </strong>{{ address && address.delegation }}</p>
+                        </div>
+                        <div class="col-3">
+                            <p><strong>Ciudad: </strong>{{ address && address.city }}</p>
+                        </div>
+                        <div class="col-3">
+                            <p><strong>Estado: </strong>{{ address && address.state }}</p>
+                        </div>
+                        <div class="col-3">
+                            <p><strong>CP: </strong>{{ address && address.cp }}</p>
                         </div>
                     </div>
                 </div>
@@ -136,6 +135,13 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+    .user-data {
+        border: 1px solid #000;
+        border-radius: 15px;
+        padding: 10px;
+        margin-bottom: 25px;
+    }
+
     .academic-background, .work-experience {
         margin-top: 10px;
         margin-bottom: 10px;
